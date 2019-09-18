c0a2b9c3ce877eadce4ca0efee23133372c22eb9
Lisa-kode <lisa6262@edu.easj.dk>
2019-09-18 10:16:25 +0200

    Initial commit

---
+++ src/Hund.java
@@ -0,0 +1,26 @@
+public class Hund {
+
+    //Deklarer variable for objekt Hund
+
+    int legs;       //hvor mange antal ben hunden har (0-4)
+    String color;   //hvilken farve hunden har
+    boolean clean;        //er hunden pottetrænet (true/false)
+    int size;       // hvor stor hunden er (10-120 cm)
+    int age;        // hvor gammel er hunden (0-15 år)
+
+
+    //Deklarerer metoder for objekt Hund
+
+    public String makeNoise(){
+        return "vuf";
+    }
+
+    public boolean run(){
+        if ()
+            legs = 4;
+    }
+
+
+
+
+}
---
+++ src/MainStart.java
@@ -0,0 +1,11 @@
+public class MainStart {
+
+    public static void main(String[] args) {
+
+        Hund goldenRetriever = new Hund();
+
+
+
+    }
+
+}
