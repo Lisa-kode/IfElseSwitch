6f687d005445e1becfd33024f6aea88c5e6ce6b4
Lisa-kode <lisa6262@edu.easj.dk>
2019-09-19 12:14:39 +0200

    En masse variabler og metoder er tilføjet Samt if og else på hundestørrelse

--- src/Hund.java
+++ src/Hund.java
@@ -4,20 +4,69 @@ public class Hund {
 
     int legs;       //hvor mange antal ben hunden har (0-4)
     String color;   //hvilken farve hunden har
-    boolean clean;        //er hunden pottetrænet (true/false)
+    boolean clean;  //er hunden pottetrænet (true/false)
     int size;       // hvor stor hunden er (10-120 cm)
     int age;        // hvor gammel er hunden (0-15 år)
+
+    //Getters and setters:
+
+    public int getLegs() {
+        return legs;
+    }
+
+    public void setLegs(int legs) {
+        this.legs = legs;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isClean() {
+        return clean;
+    }
+
+    public void setClean(boolean clean) {
+        this.clean = clean;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
 
 
     //Deklarerer metoder for objekt Hund
 
+    //Hunden kan gø
     public String makeNoise(){
         return "vuf";
     }
 
-    public boolean run(){
-        if ()
-            legs = 4;
+    //Hunden kan løbe, hvis den har fire ben
+    public boolean run(){ //der er ingen parametre i denne metode, parantesen er tom
+        if (legs == 4)
+            return true;
+        else
+            return false;
+
     }
 
 
--- src/MainStart.java
+++ src/MainStart.java
@@ -4,8 +4,52 @@ public class MainStart {
 
         Hund goldenRetriever = new Hund();
 
+        goldenRetriever.setAge(5);
+        goldenRetriever.setColor("golden");
+        goldenRetriever.setClean(true);
+        goldenRetriever.setLegs(3);
+        goldenRetriever.setSize(55);
 
+        //Kan hun løbe?
+        if (goldenRetriever.run())
+            System.out.println("Hunden løber");
+        else
+            System.out.println("Hunden kan ikke løbe");
 
+        //Hunden størreslse - er den mellem 80 og 100?
+        if (goldenRetriever.size > 80 && goldenRetriever.size < 100)
+            System.out.println("Størrelsen passer");
+        else
+            System.out.println("Størrelsen passer ikke");
+
+        // Er hunden lille, mellem eller stor?
+        if (goldenRetriever.size > 0 && goldenRetriever.size < 30)
+            System.out.println("Hunden er lille");
+        else if (goldenRetriever.size > 31 && goldenRetriever.size < 60)
+            System.out.println("Hunden er mellem");
+        else if (goldenRetriever.size > 61 && goldenRetriever.size < 90)
+            System.out.println("Hunden er stor");
+        else // her skal ikke være et statement, for dét er ro resultatet, hvis alt ovenstående ikke passer
+            System.out.println("Hunden er ekstra stor");
+
+        // ANVENDELSE AF SWITCH
+
+        int sizeInterval;
+        sizeInterval = goldenRetriever.size / 30;
+        switch (sizeInterval) {
+            case 0:
+                System.out.println("Hunden er lille");
+                break;
+            case 1:
+                System.out.println("Hunden er mellem");
+                break;
+            case 2:
+                System.out.println("Hunden er stor");
+                break;
+            case 3:
+                System.out.println("Hunden er ekstra stor");
+                break;
+        }
     }
 
 }
